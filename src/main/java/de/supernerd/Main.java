3349b22b89f3a4335ada7aa07525b2ca81ac2fca
Manuel Simon <info@der-supernerd.de>
2025-02-07 10:30:00 +0100

    Create Students

--- src/main/java/de/supernerd/Main.java
+++ src/main/java/de/supernerd/Main.java
@@ -1,4 +1,17 @@
 package de.supernerd;
 
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+
+        Student student1 = new Student("Hans", "Meyer", 333);
+        Student student2 = new Student("Felix", "Müller", 5564);
+        Student student3 = new Student("Helena", "Plath", 4456);
+
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+    }
 }
--- src/main/java/de/supernerd/Student.java
+++ src/main/java/de/supernerd/Student.java
@@ -1,4 +1,64 @@
 package de.supernerd;
 
+import java.util.Objects;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private int studentId;
+
+    public Student() {
+
+    }
+
+    public Student(String firstName, String lastName, int studentId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentId = studentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", studentId=" + studentId +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentId == student.studentId && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, studentId);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
 }
