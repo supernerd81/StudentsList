1dcfda6c8373e0d5b0e0e34eddcda701b90b0a22
Manuel Simon <info@der-supernerd.de>
2025-02-07 15:16:54 +0100

    Copnvert ArrayList to HashMap

--- src/main/java/de/supernerd/Main.java
+++ src/main/java/de/supernerd/Main.java
@@ -1,10 +1,11 @@
 package de.supernerd;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Student> students = new ArrayList<>();
+        Map<String, Student> students = new HashMap<>();
 
         Student student1 = new Student("Hans", "Meyer", 333);
         Student student2 = new Student("Felix", "Müller", 5564);
@@ -12,28 +13,28 @@ public class Main {
         Student student4 = new Student("Simone", "Zügner", 555477);
         Student student5 = new Student("Kurt", "Kussauer", 7789);
 
-        students.add(student1);
-        students.add(student2);
-        students.add(student3);
-        students.add(student4);
-        students.add(student5);
+        students.put("333", student1);
+        students.put("5564", student2);
+        students.put("4456", student3);
+        students.put("555477", student4);
+        students.put("7789", student5);
 
         School school = new School();
-        school.addStudent(student1);
-        school.addStudent(student2);
-        school.addStudent(student3);
-        school.addStudent(student4);
-        school.addStudent(student5);
+        school.addStudent("333", student1);
+        school.addStudent("5564", student2);
+        school.addStudent("4456", student3);
+        school.addStudent("555477", student4);
+        school.addStudent("7709", student5);
 
         school.printStudentList();
 
-        school.searchStudent(4456);
+        school.searchStudent("4456");
 
-        school.deleteStudent(5564);
+        school.deleteStudent("5564");
 
         System.out.println(school);
 
-        school.deleteStudent("Hans", "Meyer");
+       // school.deleteStudent("Hans", "Meyer");
 
         System.out.println(school);
     }
--- src/main/java/de/supernerd/School.java
+++ src/main/java/de/supernerd/School.java
@@ -1,78 +1,111 @@
 package de.supernerd;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class School {
-    ArrayList<Student> students;
+    Map<String, Student> students;
 
     public School() {
-        students  = new ArrayList<>();
+        students  = new HashMap<>();
     }
 
-    public void addStudent(Student student) {
-        students.add(student);
+    public void addStudent(String key, Student student) {
+        students.put(key, student);
     }
 
-    public void searchStudent(int studentId){
+//    public void searchStudent(int studentId){
+//
+//        boolean found = false;
+//
+//        for(Student obj : students) {
+//            if(obj.getStudentId() == studentId) {
+//                System.out.println("Student wurde gefunden!");
+//                System.out.println(obj);
+//                found = true;
+//                break;
+//            }
+//        }
+//
+//        if(!found) {
+//            System.out.println("Student nicht gefunden!");
+//        }
+//    }
 
-        boolean found = false;
 
-        for(Student obj : students) {
-            if(obj.getStudentId() == studentId) {
-                System.out.println("Student wurde gefunden!");
-                System.out.println(obj);
-                found = true;
-                break;
-            }
-        }
-
-        if(!found) {
-            System.out.println("Student nicht gefunden!");
+    public void searchStudent(String studentId) {
+        if(students.containsKey(studentId)) {
+            System.out.println(students.get(studentId));
         }
     }
 
-    public void deleteStudent(int studentId) {
-
-        Student deletedStudent = null;
-
-        for(Student obj : students) {
-            if(obj.getStudentId() == studentId) {
-                students.remove(obj);
-                deletedStudent = obj;
-                break;
-            }
-        }
-
-        if(deletedStudent == null) {
-            System.out.println("Student wurde nicht gelöscht, weil nicht vorhanden!");
+    public void deleteStudent(String studentId) {
+        if(students.containsKey(studentId)) {
+            students.remove(studentId);
+            System.out.println("Student wurde gelöscht!");
         } else {
-            System.out.println("Student " + deletedStudent.getFirstName() + " " + deletedStudent.getLastName() + "  wurde gelöscht!");
+            System.out.println("Student wurde nicht gefunden!");
         }
     }
 
-    public void deleteStudent(String firstName, String lastName) {
-        boolean found = false;
+//    public void deleteStudent(int studentId) {
+//
+//        Student deletedStudent = null;
+//
+//        for(Student obj : students) {
+//            if(obj.getStudentId() == studentId) {
+//                students.remove(obj);
+//                deletedStudent = obj;
+//                break;
+//            }
+//        }
+//
+//        if(deletedStudent == null) {
+//            System.out.println("Student wurde nicht gelöscht, weil nicht vorhanden!");
+//        } else {
+//            System.out.println("Student " + deletedStudent.getFirstName() + " " + deletedStudent.getLastName() + "  wurde gelöscht!");
+//        }
+//    }
 
-        for(Student obj : students) {
-            if(obj.getFirstName().equals(firstName) && obj.getLastName().equals(lastName)) {
-                deleteStudent(obj.getStudentId());
-                found = true;
-                break;
-            }
-        }
 
-        if(!found) {
-            System.out.println("Student wurde nicht gelöscht, weil nicht vorhanden!");
-        }
-    }
+
+//    public void deleteStudent(String firstName, String lastName) {
+//        boolean found = false;
+//
+//        for(Student obj : students) {
+//            if(obj.getFirstName().equals(firstName) && obj.getLastName().equals(lastName)) {
+//                deleteStudent(obj.getStudentId());
+//                found = true;
+//                break;
+//            }
+//        }
+//
+//        if(!found) {
+//            System.out.println("Student wurde nicht gelöscht, weil nicht vorhanden!");
+//        }
+//    }
 
 
     public void printStudentList() {
         System.out.println(students);
     }
 
-    public School(ArrayList<Student> students) {
+    public School(Map<String, Student> students) {
         this.students = students;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        School school = (School) o;
+        return Objects.equals(students, school.students);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(students);
     }
 
     @Override
