dd197a384ab6825befc13e245270b80b421ffa54
Manuel Simon <info@der-supernerd.de>
2025-02-07 11:53:49 +0100

    Create class School

--- src/main/java/de/supernerd/Main.java
+++ src/main/java/de/supernerd/Main.java
@@ -9,16 +9,32 @@ public class Main {
         Student student1 = new Student("Hans", "Meyer", 333);
         Student student2 = new Student("Felix", "Müller", 5564);
         Student student3 = new Student("Moritz", "Kussauer", 4456);
+        Student student4 = new Student("Simone", "Zügner", 555477);
+        Student student5 = new Student("Kurt", "Kussauer", 7789);
 
         students.add(student1);
         students.add(student2);
         students.add(student3);
+        students.add(student4);
+        students.add(student5);
 
         School school = new School();
         school.addStudent(student1);
         school.addStudent(student2);
         school.addStudent(student3);
+        school.addStudent(student4);
+        school.addStudent(student5);
 
         school.printStudentList();
+
+        school.searchStudent(4456);
+
+        school.deleteStudent(5564);
+
+        System.out.println(school);
+
+        school.deleteStudent("Hans", "Meyer");
+
+        System.out.println(school);
     }
 }
--- src/main/java/de/supernerd/School.java
+++ src/main/java/de/supernerd/School.java
@@ -13,7 +13,72 @@ public class School {
         students.add(student);
     }
 
+    public void searchStudent(int studentId){
+
+        boolean found = false;
+
+        for(Student obj : students) {
+            if(obj.getStudentId() == studentId) {
+                System.out.println("Student wurde gefunden!");
+                System.out.println(obj);
+                found = true;
+                break;
+            }
+        }
+
+        if(!found) {
+            System.out.println("Student nicht gefunden!");
+        }
+    }
+
+    public void deleteStudent(int studentId) {
+
+        Student deletedStudent = null;
+
+        for(Student obj : students) {
+            if(obj.getStudentId() == studentId) {
+                students.remove(obj);
+                deletedStudent = obj;
+                break;
+            }
+        }
+
+        if(deletedStudent == null) {
+            System.out.println("Student wurde nicht gelöscht, weil nicht vorhanden!");
+        } else {
+            System.out.println("Student " + deletedStudent.getFirstName() + " " + deletedStudent.getLastName() + "  wurde gelöscht!");
+        }
+    }
+
+    public void deleteStudent(String firstName, String lastName) {
+        boolean found = false;
+
+        for(Student obj : students) {
+            if(obj.getFirstName().equals(firstName) && obj.getLastName().equals(lastName)) {
+                deleteStudent(obj.getStudentId());
+                found = true;
+                break;
+            }
+        }
+
+        if(!found) {
+            System.out.println("Student wurde nicht gelöscht, weil nicht vorhanden!");
+        }
+    }
+
+
     public void printStudentList() {
         System.out.println(students);
+    }
+
+    public School(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "students=" + students +
+                '}';
     }
 }
